dba1aa16d69cc4efb34015b8a0401a08efde8580
Ben Laurie <ben@links.org>
2013-10-26 18:45:12 +0100

    USB support.

--- sw/airborne/modules/gps/gps_ubx_ucenter.c
+++ sw/airborne/modules/gps/gps_ubx_ucenter.c
@@ -278,7 +278,8 @@ static inline void gps_ubx_ucenter_config_nav(void)
 #define GPS_PORT_SPI   0x04
 
 #ifndef GPS_PORT_ID
-#define GPS_PORT_ID GPS_PORT_UART1
+//#define GPS_PORT_ID GPS_PORT_UART1
+#define GPS_PORT_ID GPS_PORT_USB
 #endif
 
 #define __UBX_GPS_BAUD(_u) _u##_BAUD
